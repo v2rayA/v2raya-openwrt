278080651bf5db03302a074f2e744c89d649a614
Akira Yamazaki <chihsiao@outlook.com>
2023-02-26 20:48:42 +0800

    luci-app-v2raya: fix the url to v2raya
    When visiting luci with https, the url to v2raya will be of https, which is unexpected.

--- luci-app-v2raya/htdocs/luci-static/resources/view/v2raya/config.js
+++ luci-app-v2raya/htdocs/luci-static/resources/view/v2raya/config.js
@@ -35,7 +35,7 @@ function renderStatus(isRunning, port) {
 	var renderHTML;
 	if (isRunning) {
 		var button = String.format('&#160;<a class="btn cbi-button" href="%s:%s" target="_blank" rel="noreferrer noopener">%s</a>',
-			window.location.origin, port, _('Open Web Interface'));
+			window.location.hostname, port, _('Open Web Interface'));
 		renderHTML = spanTemp.format('green', _('v2rayA'), _('RUNNING')) + button;
 	} else {
 		renderHTML = spanTemp.format('red', _('v2rayA'), _('NOT RUNNING'));
